1e7958fffd79cb3a6f779a51b2ec66c551da1827
Dreamarchs <devenw5@gmail.com>
2020-10-05 17:19:24 -0400

    Created UI to show controls that are disabled after 5 seconds, made the boomerang the correct model and placed it in the hand

---
+++ Assets/Scripts/UI/MoveImage.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveImage : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(TurnImageOff());
+    }
+
+    IEnumerator TurnImageOff()
+    {
+        yield return new WaitForSeconds(5f);
+        this.gameObject.SetActive(false);
+    }
+}
